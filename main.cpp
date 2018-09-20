4372e7006c7c97be0f3ecc8f818030ad0d539e80
Ochrazy <johannes.wambach@gmail.com>
2018-09-20 16:55:22 +0200

    removed obsolete plugin paths in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::addLibraryPath("C:\\Qt\\5.11.1\\Src\\qtbase\\plugins");
-    QCoreApplication::addLibraryPath("//Users//Ochrazy//Qt//5.11.1//Src//qtbase//plugins");
     QApplication a(argc, argv);
     MachineClicker w;
     w.show();
